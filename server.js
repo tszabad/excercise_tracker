683f0b91b1073b198d6b29049107a72fe3723326
Glitch (yielding-cookie) <none>
2019-02-27 12:21:24 +0000

    🌮🛩 Checkpoint ./server.js:2457780/5433 ./package.json:2457780/1126 ./views/index.html:2457780/3844 ./public/style.css:2457780/1615

--- server.js
+++ server.js
@@ -3,45 +3,189 @@ const app = express()
 const bodyParser = require('body-parser')
 
 const cors = require('cors')
-
+const helmet = require('helmet')
+const path = require('path');
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+var Schema = mongoose.Schema;
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(
+  () => {
+    console.log("mongo opened")
+    
+  },
+  err => {
+    console.error("### error starting mongo")
+    console.error(err)
+  }
+);
+
 
 app.use(cors())
+app.use(helmet())
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 
-// Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
+const userSchema = new mongoose.Schema(
+  {username: String});
 
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage
+const exerciseSchema = new mongoose.Schema({
+  userId: {type:String, required: true},
+  username: {type: String, required: true},
+  description: {type:String,required: true},
+  duration: {type:Number,required: true},
+  date: Number
+});
 
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
+const User = mongoose.model('User', userSchema);
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+const dateToNumber = (date) => {
+  if(date) {
+    const year = date.slice(0,4);
+    const month = date.slice(5,7);
+    const day = date.slice(8);
+    return Number(year + month + day);
   } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
+    return null;
   }
-  res.status(errCode).type('txt')
-    .send(errMessage)
+}
+
+const dateFromNumber = (date) => {
+  let newDate = date.toString();
+  const year = newDate.slice(0,4);
+  const month = newDate.slice(4,6);
+  const day = newDate.slice(6);
+  return `${year}-${month}-${day}`;
+}
+
+app.use('/api/exercise/new-user', (req, res, next) => {
+  const username = req.body.username
+  User.find({username}, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      if (result.length === 0) {
+        next();
+      } else {
+        res.send('duplicate username not allowed');
+      }
+    }
+  })
 })
+
+app.use('/api/exercise/log', (req,res,next) => {
+  const _id = req.query.userId;
+  User.findById(_id, (err, result) => {
+    if(err) {
+      res.send('User not found');
+    } else {
+      next();
+    }
+  })
+})
+
+
+app.get('/api/exercise/log', (req,res) => {
+  const userId = req.query.userId
+  const from = dateToNumber(req.query.from) || 0;
+  const to = dateToNumber(req.query.to) || 99999999;
+  const limit = (Math.abs(0 - (req.query.limit || 0)));
+  Exercise.find({
+    userId,
+    date: {$gt: from, $lt: to},
+  }, {}, {limit}, (err,result) => {
+    if(err) {
+      res.send(err);
+    } else {
+      res.send(result);
+      console.log(result);
+    }
+  })
+})
+
+app.use('/api/exercise/user', (req,res,next) => {
+  const username = req.query.username;
+  
+  User.find({username}, (err, result) => {
+    if(err) {
+      res.send('User not found');
+    } else {
+      next();
+    }
+  })
+})
+
+
+app.get('/api/exercise/user', (req,res) => {
+ 
+  const username = req.query.username
+  User.find({username}
+      ,(err,result) => {
+    if(err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  })
+})
+
+app.post('/api/exercise/new-user', (req,res) => {
+  const username = req.body.username;
+  User.create({username}, (err,user) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(user);
+    }
+  })
+})
+
+app.post('/api/exercise/add', (req,res) => {
+  var {userId, description, duration, date} = req.body;
+  if (date.length === 0) {
+  let newDate = new Date;
+  var dd = newDate.getDate();
+  var mm = newDate.getMonth()+1; 
+  var yyyy = newDate.getFullYear();
+    if(dd<10) {
+    dd = '0'+dd} 
+if(mm<10) {
+    mm = '0'+mm} 
+    date = Number(yyyy+mm+dd);
+    console.log(date);
+  }else{
+  date = dateToNumber(date);}
+  User.findById(userId, (err, user) => {
+    if (err) {
+      res.send('user ID not found')
+    } else {
+        Exercise.create({userId: userId, username: user.username, description, duration, date}, (err, exercise) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send(`{"userId": "${userId}", "username": "${user.username}", "description": "${description}", "duration": "${duration}", "date": "${dateFromNumber(date)}"}`);
+        }
+  })
+      
+     
+    }
+  })
+  
+})
+
+
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
